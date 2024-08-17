2ba0b9190f893a1d91d9359778e1a43c8611eb68
rajib5570 <rajib5570@gmail.com>
2024-08-17 15:28:15 +0600

    cors config added

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(
+    cors({
+      origin: [
+        "http://localhost:5173"
+      ]
+    })
+  );
